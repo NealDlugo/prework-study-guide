81ca7e6816334a826cab6f7ab05011071671193d
Neal <nealdlugokinski1@gmail.com>
2023-01-10 12:29:21 -0500

    git commit

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,4 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-<<<<<<< HEAD
-function listTopics() {
-    for (var x = 0; x < topics.length; x++) {
-        console.log(topics[x]);
-    }
-}
-function selectTopic() {
-  if (randomTopic === 'HTML') {
-    console.log("Let's study HTML!");
-  } else if (randomTopic === 'CSS') {
-    console.log("Let's study CSS!");
-  } else if (randomTopic === 'Git') {
-    console.log("Let's study Git!");
-  } else if (randomTopic === 'JavaScript') {
-    console.log("Let's study JavaScript!");
-  } else {
-    console.log('Please try again!');
-  }
-}
-  listTopics()
-  selectTopics()
-=======
 
 for (var x = 0; x < topics.length; x++) {
  console.log(topics[x]);
@@ -39,4 +17,3 @@ if (topic === 'HTML') {
  console.log('Please try again!');
 }
 */
->>>>>>> c0d654095e959ae4112e31cd97c48dad67bab168
